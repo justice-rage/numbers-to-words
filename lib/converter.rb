1a2e01af8684f197190262142c4c5f0bd6e1d69b
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-04-13 20:07:16 -0700

    pass test 5

--- lib/converter.rb
+++ lib/converter.rb
@@ -13,6 +13,16 @@ def converter(number)
     8 => 'eight',
     9 => 'nine',
     10 => 'ten',
+    11 => 'eleven',
+    12 => 'twelve',
+    13 => 'thirtenn',
+    14 => 'fourteen',
+    15 => 'fifteen',
+    16 => 'sixteen',
+    17 => 'seventeen',
+    18 => 'eighten',
+    19 => 'nineteen',
+    20 => 'twenty',
   }
   numbers_to_words.fetch(number)
 end
